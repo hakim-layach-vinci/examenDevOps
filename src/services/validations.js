520d9f5ac1f1f89b34f89a988040d93e0950e2ec
hakim-layach-vinci <hakim.layach@student.vinci.be>
2024-01-15 17:31:43 +0100

    add:is valid check special caracter

--- src/services/validations.js
+++ src/services/validations.js
@@ -1,6 +1,17 @@
 // TODO: Refactor validation function to follow the defined validations rules
 function isValid(gamerTag) {
-    if (gamerTag.lenght >= 8) return true;
-    return false;
+    if (gamerTag.length < 8) {
+        return "Invalid - gamertag length must be at least 8 characters";
+    }
+
+    const specialCharacters = ['&', '$', '!', 'è', '§', 'à', '_'];
+    if (!specialCharacters.some(char => gamerTag.includes(char))) {
+        return "Invalid - gamertag must contain at least a special character";
+    }
+
+    
+
+    // Si toutes  conditions sont remplies, le gamerTag est valide
+    return true;
 }
 exports.isValid = isValid;
